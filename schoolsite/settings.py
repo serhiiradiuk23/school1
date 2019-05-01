63dedd057e349b502169a2d12987aefc1b983782
serhiiradiuk23 <49027888+serhiiradiuk23@users.noreply.github.com>
2019-05-01 13:25:25 +0300

    Add files via upload

--- schoolsite/settings.py
+++ schoolsite/settings.py
@@ -119,12 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/school1.pythonanywhere.com/static/'
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/school1.pythonanywhere.com/static/'
+# SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
- STATICFILES_DIRS = (
-     os.path.join(SITE_ROOT, 'static/'),
- )
+ # STATICFILES_DIRS = (
+ #     os.path.join(SITE_ROOT, 'static/'),
+ # )
 
 TIME_ZONE = 'Europe/Kiev'
